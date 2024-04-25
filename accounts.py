215f83e6b5b348d456fe3d145e99b8d3c2538856
Finbar Dennehy <finny.d@gmail.com>
2024-04-25 09:45:24 +0100

    typos correct and re-ordered modification

--- accounts.py
+++ accounts.py
@@ -6,19 +6,6 @@ Resources:
     I referenced https://stackoverflow.com/questions/509211/how-slicing-in-python-works for more information on slicing in python.
     I referenced https://stackoverflow.com/questions/38273353/how-to-repeat-individual-characters-in-strings-in-python to learn how to repeat letters through multiplication.
 '''
-
-# First program below, commented out (lines 12 - 19).
-'''
-# Ask user to enter 10 digit account number, defaulting to type str.
-account_number = input("Please enter a 10 digit account number: ")
-
-# Hard code 6 Xs and concatenate with a slice of the last 4 digits of the account number string.
-redacted_account_number = "XXXXXX" + account_number[7:10]
-
-# Print the redacted account number.
-print(redacted_account_number)
-'''
-
 # Modified program below.
 '''
 Modify the program to deal with account numbers of any length
@@ -29,13 +16,26 @@ Assumptions:
 '''
 
 # Ask user to enter their account number.
-account_number2 = input("Please enter your account number: ")
+account_number = input("Please enter your account number: ")
 
-# Calculate how many Xs are required by subtracting 4 from the length of the account number. Crate a string of this number of Xs and assign to redacted variable.
-redacted = (len(account_number2) - 4)*'X'
+# Calculate how many Xs are required by subtracting 4 from the length of the account number. Create a string of this number of Xs and assign to redacted variable.
+redacted = (len(account_number) - 4)*'X'
 
 # Concatenate Xs (if account > 4 digits) and the last 4 digits of the account number 
-redacted_account_number2 = redacted + (account_number2[-4:])
+redacted_account_number = redacted + (account_number[-4:])
+
+# Print the redacted account number.
+print(redacted_account_number)
+
+# Original program below
+
+'''
+# Ask user to enter 10 digit account number, defaulting to type str.
+account_number2 = input("Please enter a 10 digit account number: ")
+
+# Hard code 6 Xs and concatenate with a slice of the last 4 digits of the account number string.
+redacted_account_number2 = "XXXXXX" + account_number2[7:10]
 
 # Print the redacted account number.
 print(redacted_account_number2)
+'''
